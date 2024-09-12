4c311f71b8847810281824083ac7cde392f086e0
vkarelina <v.karelina@tg.dunice.net>
2024-09-12 10:18:09 +0300

    Rename Header.tsx to header.tsx and rename css for header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,41 @@
+import "./header.css";
+
+import { useEffect, useRef } from "react";
+
+import { useTabStore } from "../../store";
+import { themesMock } from "../../mockdata";
+
+const Header = () => {
+  const refCurrentElement = useRef<string | null>(null);
+  
+  const setFilter = useTabStore((state) => state.setFilter);
+  const setTheme = useTabStore((state) => state.setTheme);
+  const themes = useTabStore((state) => state.themes);
+  const filter = useTabStore((state) => state.filter);
+
+  useEffect(() => {
+    setTheme(themesMock);
+  },[])
+
+  const onTabCLick = (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.target;
+
+    if (target instanceof HTMLElement && target.dataset.tab) {
+      if (filter === target.innerText) return;
+      refCurrentElement.current = target.innerText;
+      setFilter(refCurrentElement.current);
+    }
+  };
+
+  return (
+    <div className="wrapper-header" onClick={onTabCLick}>
+      { themes && themes.map((theme, index) => (
+        <div key={index} data-tab="true">
+          {theme}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Header;
