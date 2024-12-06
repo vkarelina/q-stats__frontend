79f009a06f8aee17b004cd9c80f893691a80a46d
vkarelina <v.karelina@tg.dunice.net>
2024-12-06 17:58:32 +0300

    feat: add update topic question

--- src/components/answers-table-row/answers-table-row.tsx
+++ src/components/answers-table-row/answers-table-row.tsx
@@ -1,6 +1,10 @@
+import cn from 'classnames';
+import { useRef, useState } from 'react';
+
 import MenuVerticalIcon from '../../assets/icons/menu-vertical.svg';
 import DropdownList from '../../components-ui/dropdown-list/dropdown-list';
 import useAnswers from '../../store/answers';
+import useQuestion from '../../store/question';
 import useTopic from '../../store/topic';
 import useUser from '../../store/user';
 import { Question } from '../../types';
@@ -15,9 +19,14 @@ interface AnswersTableRowProps {
 }
 
 const dropdownList = [
-  { id: 1, label: 'Edite' },
+  { id: 1, label: 'Edit' },
   { id: 2, label: 'Delete' },
 ];
+
+const enum MenuList {
+  Edit = 1,
+  Delete = 2,
+}
 
 const AnswersTableRow = ({
   question,
@@ -29,6 +38,10 @@ const AnswersTableRow = ({
   const answers = useAnswers.use.answers();
 
   const fetchAnswers = useAnswers.use.fetchAnswers();
+  const fetchUpdateTopicQuestion = useQuestion.use.fetchUpdateTopicQuestion();
+
+  const [isShowTextarea, setIsShowTextarea] = useState(false);
+  const refText = useRef<HTMLTextAreaElement | null>(null);
 
   const sortedAnswers = answers.sort((a, b) => {
     const dateA = new Date(a.date.split('-').reverse().join('-')).getTime();
@@ -46,14 +59,43 @@ const AnswersTableRow = ({
   };
 
   const getItem = (id: number) => {
-    // TODO: Пример использования списка DropdownList
-    console.log(id);
+    switch (id) {
+      case MenuList.Edit:
+        setIsShowTextarea(true);
+        break;
+      case MenuList.Delete:
+        console.log(id);
+        break;
+      default:
+        console.log(id);
+    }
+  };
+
+  const handleUpdateText = (question: Question) => {
+    if (!refText.current || refText.current.value === '') return;
+
+    fetchUpdateTopicQuestion(
+      { text: refText.current.value },
+      question.id,
+      topic?.id,
+      refreshQuestions,
+    );
+
+    setIsShowTextarea(false);
   };
 
   return (
     <tr className={styles.row}>
       <td className={styles.question}>
-        <p>{`${index + 1}. ${question.text}`}</p>
+        <p className={cn({ [styles.close]: isShowTextarea })}>
+          {`${index + 1}. ${question.text}`}
+        </p>
+        <textarea
+          className={cn({ [styles.close]: !isShowTextarea })}
+          defaultValue={question.text}
+          onBlur={() => handleUpdateText(question)}
+          ref={refText}
+        />
         <DropdownList
           items={dropdownList}
           getItemList={getItem}
